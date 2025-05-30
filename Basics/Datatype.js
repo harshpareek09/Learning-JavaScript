2a0b344ca1807164713acd0f49c1062475252411
Harsh Pareek <pareekharsh9305@gmail.com>
2025-05-30 13:01:10 +0000

    Primitive Dataypes

---
+++ Basics/Datatype.js
@@ -0,0 +1,29 @@
+//Primitive Types
+let num = 25;
+console.log("Data Type of 25 is:"+typeof num);
+
+let str = "Harsh Pareek";
+console.log("Data Type of 'Harsh Pareek' is:"+typeof str);
+
+let isCorrect = true;
+console.log("Data Type of isCorrect is:"+typeof isCorrect);
+
+let r_no;
+console.log("Data Type of r_no is:"+typeof r_no);
+
+let color = null;
+console.log("Data Type of color is:"+typeof color);
+
+let biggerNumber = 2222222222222222;
+console.log("Data Type of biggerNumber is:"+typeof biggerNumber);
+
+biggerNumber = BigInt("22222233");
+console.log("Data Type of biggerNumber is:"+typeof biggerNumber);
+
+
+
+
+
+
+
+
