0efc6e349b9b45390e250a0c43309aa657869c25
Harsh Pareek <pareekharsh9305@gmail.com>
2025-05-30 10:38:49 +0000

    variables

---
+++ Basics/Variables.js
@@ -0,0 +1,14 @@
+//Const cant be redeclare[False]  reintialize[False]
+const rollNo = 1;
+
+//redeclare[False]  reintialize[True]
+let Names= "Harsh Pareek"
+
+//redeclare[True]  reintialize[True]
+var subject = "Computer Science"
+
+// Without keyword treated as let
+examName = "Sessionals"
+
+//To visualize data in table form
+console.table([rollNo ,Names, subject, examName])
